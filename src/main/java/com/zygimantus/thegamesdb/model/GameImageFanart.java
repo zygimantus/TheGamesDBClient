b9bb64490ba7cecbe1c87df070a27ceb1d589213
Zygimantus <zygimantus@gmail.com>
2018-05-25 17:38:38 +0300

    Fix: GameImageFanart

--- src/main/java/com/zygimantus/thegamesdb/model/GameImageFanart.java
+++ src/main/java/com/zygimantus/thegamesdb/model/GameImageFanart.java
@@ -19,14 +19,4 @@ public class GameImageFanart extends GameImage {
         width = original.getHeight();
     }
 
-    @Override
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    @Override
-    public String getUrl() {
-        return url;
-    }
-
 }
